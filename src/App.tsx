42de87c46e3e37827b491f026ea61b75952fd7b6
Sabyrka <sabyrkaway@gmail.com>
2026-01-18 20:07:56 +0500

    refactor: add track selection state and reset option

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,13 @@ interface TrackListItemAttributes {
 }
 
 interface TrackListItemResource {
-  id: number
+  id: string
   attributes: TrackListItemAttributes
 }
 
 export const App = () => {
   const [tracks, setTracks] = useState<TrackListItemResource[] | null>(null)
+  const [selectedTrackId, setSelectedTrackId] = useState<string | null>(null)
 
   useEffect(() => {
     fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
@@ -30,11 +31,19 @@ export const App = () => {
     <>
       <h1>MusicFun</h1>
 
+      <button onClick={() => setSelectedTrackId(null)}>
+        Reset selection
+      </button>
+
       {tracks === null && <div>Loading...</div>}
       {tracks?.length === 0 && <div>No tracks available</div>}
       {tracks && <ul>
         {tracks.map((track) => (
-          <li key={track.id}>
+          <li
+            key={track.id}
+            onClick={() => setSelectedTrackId(track.id)}
+            style={{ border: `1px solid ${track.id === selectedTrackId ? '#646cff' : 'transparent'}` }}
+          >
             <div>{track.attributes.title}</div>
             <audio controls src={track.attributes.attachments[0].url} />
           </li>
