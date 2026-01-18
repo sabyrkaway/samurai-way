bfd6ac23032a4e5c9db37bb8bd4aa269a6b5f7c8
Sabyrka <sabyrkaway@gmail.com>
2026-01-19 01:04:22 +0500

    refactor: remove pageSize parameter from playlist API

--- src/features/playlist/playlist.api.ts
+++ src/features/playlist/playlist.api.ts
@@ -1,6 +1,6 @@
 import { HEADERS } from '@/constants'
 
 export const getPlaylist = async () => {
-  const res = await fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks?pageSize=5', { headers: HEADERS })
+  const res = await fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', { headers: HEADERS })
   return await res.json()
 }
