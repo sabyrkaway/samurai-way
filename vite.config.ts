b6a116572ae6d0b197664e09b24506f943b49d09
Sabyrka <sabyrkaway@gmail.com>
2026-01-19 08:38:44 +0500

    chore: update Vite base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'samurai-way',
+  base: '/samurai-way',
   plugins: [react()],
   resolve: {
     alias: {
